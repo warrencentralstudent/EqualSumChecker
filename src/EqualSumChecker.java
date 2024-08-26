d687ddd1155ea4c19fe51af9c3dfcd55931eeb0f
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-26 11:38:36 -0400

    yeah

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    public static boolean hasEqualSum(int num1, int num2, int num3) {
+        int hello = (num1 + num2);
+        if(hello == num3){
+            return true;
+        }
+        return false;
+    }
+
+}
